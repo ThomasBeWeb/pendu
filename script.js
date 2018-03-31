9f6b1a83faae57fe1792ee9bbdec4666b60f413f
Tom <utilisateur@macbook-1.home>
2018-03-31 13:59:36 +0200

    V3

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-//Liste des mots à trouver:
+//Liste des mots disponibles:
 var listeMots = ["tortue","vacances","pouet","cafetiere"]
 
-//Mot à trouver
+//Declaration des variables mots
 var motATrouver = "";
-
-//Mot affiché
 var motEnCours = "";
+
+//Liste des lettres déjà testées
+var listeLettres = [];
+
 
 //Fonction nouveau jeu
 function startGame(){
 
-    //Reset des mots
+    //Reset des mots et liste
     motATrouver = "";
     motEnCours = "";
+    listeLettres = [];
 
     //Tirage d'un index
     let index = Math.floor(Math.random() * listeMots.length);
@@ -26,11 +29,14 @@ function startGame(){
 
     }
 
+    console.log(motATrouver);
+
     showWord(motEnCours);
 
 }
 
-//Fonction affichage mot
+
+//Fonction affichage mot et liste des lettres utilisées
 function showWord(mot){
 
     //Construction du mot à afficher
@@ -44,5 +50,79 @@ function showWord(mot){
 
     //Ajout du mot
     document.getElementById("motAtrouver").innerHTML = motToScreen;
+
+    //Affichage des lettres de la liste
+    motToScreen = "";
+
+    for(i = 0 ; i < listeLettres.length ; i++){
+
+        motToScreen += (listeLettres[i] + ", ");
+
+    }
+
+    document.getElementById("lettresPasOk").innerHTML = motToScreen;
 }
 
+
+//Touche entrée dans champ text
+var textInput = document.getElementById("testLettre")
+
+textInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+
+        //Test de la lettre
+        testLettre(textInput.value);
+
+        //Reset de l'input
+        textInput.value = "";
+    }
+});
+
+
+//Fonction test lettre
+function testLettre(lettre){
+
+    //Verification si lettre déjà testée
+
+    var flag = true;
+
+    for(i = 0 ; i < listeLettres.length ; i++){
+
+        if(listeLettres[i] === lettre){
+
+            alert("Lettre déjà testée, faire un autre choix svp");
+            flag = false;
+            break;
+        }
+    }
+
+    //Test lettre dans mot à trouver
+    if(flag === true){
+
+        //NewMot
+
+        var newMot = "";
+
+        for(i = 0 ; i < motATrouver.length ; i++){
+
+            if(motATrouver[i] === lettre){
+
+                //Ajout de la lettre dans mot à afficher              
+                newMot += lettre;
+
+            }else{
+                newMot += motEnCours[i];
+            }
+        }
+
+        motEnCours = newMot;
+
+        //Ajout de la lettre dans la liste des lettres téstées
+        listeLettres.push(textInput.value);
+
+        showWord(motEnCours);
+    }
+}
+
+
+//Fonction affichage
